92a3f49d64a46105f723d0131485fc147a7a7bd2
makkob <maximkabig945@gmail.com>
2023-01-18 19:34:54 +0200

    n

--- src/componets/Newspage/Newspage.js
+++ src/componets/Newspage/Newspage.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Newspage(props) {
-  console.log(props.match);
-  return <div>Newspage</div>;
+  // const dispatch = useDispatch();
+  const [itemState, onSetItem] = useState();
+  const newsState = useSelector((state) => state.newsListState);
+  const { news } = newsState;
+
+  useEffect(() => {
+    const item = news.filter(({ id }) => {
+      return id == props.match.params.id;
+    });
+
+    onSetItem(...item);
+  }, []);
+
+  return (
+    <>
+      {itemState && <h3 key={itemState.id}>{itemState.title}</h3>}
+
+      {itemState && <a href={itemState.url}></a>}
+      {itemState && <p> {itemState.time}</p>}
+      {itemState && <p> {itemState.by}</p>}
+    </>
+  );
 }
